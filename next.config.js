ac029800fd26f752a939c583a8626bc2c08a27b4
Imron Mirzarasulov <imronimronov32@gmail.com>
2022-11-29 15:15:42 +0500

    first commit

--- next.config.js
+++ next.config.js
@@ -1,4 +1,23 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
-  reactStrictMode: true,
+	reactStrictMode: true,
+	poweredByHeader: false,
+	env: {
+		APP_URL: process.env.REACT_APP_URL
+	},
+	images: {
+		domains: ['localhost']
+	},
+	async rewrites() {
+		return [
+			{
+				source: '/api/:path*',
+				destination: 'http://localhost:4200/api/:path*'
+			},
+			{
+				source: '/uploads/:path*',
+				destination: 'http://localhost:4200/uploads/:path*'
+			}
+		]
+	}
 }
